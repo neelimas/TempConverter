b451c83703cc6e5cf0ab58bb42efbed545aa3859
user <user@users-Mac-mini-127.local>
2013-11-11 09:26:13 -0600

    Initial Commit

---
+++ TempConverter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TempConverter
+//
+//  Created by user on 11/11/13.
+//  Copyright (c) 2013 Classroom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TempConverter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TempConverter
+//
+//  Created by user on 11/11/13.
+//  Copyright (c) 2013 Classroom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
